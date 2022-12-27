ee48cd0d0ec26ab26fc161bc96b3a291644c66dd
Akshara-22 <akshara.deshmukh2802@gmail.com>
2022-12-27 15:34:36 +0530

    final commit

---
+++ src/Demo1.java
@@ -0,0 +1,29 @@
+
+
+public class Demo1 {
+
+
+
+	public static void main(String[] args) {
+
+		System.out.println("HI");
+		int num1=0,num2=1,num3,range=5;
+		for(int i=1;i<=range;i++){
+			System.out.println(num1);
+			num3=num1+num2;
+			num1=num2;
+			num2=num3;
+
+
+		}
+
+	}
+
+
+
+
+
+
+
+
+}
