5b1801ad8464e71c62ed768c3b6ea55c3631431e
Akshara-22 <akshara.deshmukh2802@gmail.com>
2022-12-27 14:38:49 +0530

    this is commit from American Timezone

--- src/StringReplace.java
+++ src/StringReplace.java
@@ -6,9 +6,13 @@ public class StringReplace {
 
 		String str = new String("View timesheet create timesheet");
 		System.out.println( "String before replace: "+ str);
+		System.out.println( "String before replace 1: "+ str);
+
 		
 		str = str.replaceAll( "timesheet" , "record" );
 		System.out.println( "The String after replece : "+str );
+		System.out.println( "The String after replece 1: "+str );
+
 	}
 }
 
