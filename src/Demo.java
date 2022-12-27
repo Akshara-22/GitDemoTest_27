afcb37502ea99ddcf1c3389c8f613214e9047ac7
Akshara-22 <akshara.deshmukh2802@gmail.com>
2022-12-27 15:30:01 +0530

    commited demo

--- src/Demo.java
+++ src/Demo.java
@@ -5,7 +5,7 @@ public class Demo {
 
 	public static void main(String[] args) {
 
-
+System.out.println("HI");
 		int num1=0,num2=1,num3,range=5;
 		for(int i=1;i<=range;i++){
 			System.out.println(num1);
