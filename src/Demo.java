f5f093d4c8c96061616ab0a95a38e2058edc22d9
Akshara-22 <akshara.deshmukh2802@gmail.com>
2022-12-27 15:25:47 +0530

    Asian Timezone commited Demo

---
+++ src/Demo.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Demo {
+
+	public static void main(String[] args) {
+
+
+		int num1=0,num2=1,num3,range=5;
+		for(int i=1;i<=range;i++){
+			System.out.println(num1);
+            num3=num1+num2;
+            num1=num2;
+            num2=num3;
+            
+
+		}
+	}
+}
+
+
+
+
+
+
+
